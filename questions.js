361df5e32e5b3acfb21a0608843398573a78e8fc
LilCC <manosa.carla@gmail.com>
2019-09-26 08:52:44 -0700

    created questions for the quiz

---
+++ questions.js
@@ -0,0 +1,23 @@
+let questions = [
+    {
+        question: "How do we reference a div on css?",
+        choicesArr: [".", "#", "?", "!"],
+    } , {
+        question: "To make our webpage neutral before we add or make changes, what document should we always add first?",
+        choicesArr: ["reset.css", "css", "html", "javascript"],
+    }, {
+        question: "What do we use to program the behavior of a web page?",
+        choicesArr: ["javascript", "html", "css", "VS Code"],
+
+    }, {
+        question: "What the first term in an array?",
+        choicesArr: ["Index 0", "Index 1", "Index 2", "Index 3"],
+    }, {
+        question: "What is the third term in an array?",
+        choicesArr: ["Index 2", "Index 1", "Index 0", "Index 3"],
+    }, {
+        question: "What kind of data type consists of true or false values?",
+        choicesArr: ["Boolean", "String", "if", "else"],
+    }
+    
+]
