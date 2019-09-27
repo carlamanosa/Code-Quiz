994d04007d5bb746fa151a308707a9fde58d2110
LilCC <manosa.carla@gmail.com>
2019-09-26 19:26:33 -0700

    progressively completing java

---
+++ index.js
@@ -0,0 +1,13 @@
+const start = document.getElementById("start-page");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+const progress = document.getElementById("progress");
+const scoreContainer = document.getElementById("storeContainer");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+
--- questions.js
+++ questions.js
@@ -2,22 +2,27 @@ let questions = [
     {
         question: "How do we reference a div on css?",
         choicesArr: [".", "#", "?", "!"],
+        correct: questions.choicesArr[0],
     } , {
         question: "To make our webpage neutral before we add or make changes, what document should we always add first?",
-        choicesArr: ["reset.css", "css", "html", "javascript"],
+        choicesArr: ["html", "css", "reset.css", "javascript"],
+        correct: questions.choicesArr[2],
     }, {
         question: "What do we use to program the behavior of a web page?",
         choicesArr: ["javascript", "html", "css", "VS Code"],
-
+        correct: questions.choicesArr[0],
     }, {
         question: "What the first term in an array?",
-        choicesArr: ["Index 0", "Index 1", "Index 2", "Index 3"],
+        choicesArr: ["Index 2", "Index 1", "Index 0", "Index 3"],
+        correct: questions.choicesArr[2],
     }, {
         question: "What is the third term in an array?",
-        choicesArr: ["Index 2", "Index 1", "Index 0", "Index 3"],
+        choicesArr: ["Index 3", "Index 1", "Index 0", "Index 2"],
+        correct: questions.choicesArr[3],
     }, {
         question: "What kind of data type consists of true or false values?",
         choicesArr: ["Boolean", "String", "if", "else"],
+        correct: questions.choicesArr[0],
     }
     
 ]
